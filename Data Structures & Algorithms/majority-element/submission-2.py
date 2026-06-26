32ed29073c6f363a00c7e489fa3d3d8a348eb9da
Vishoni <sahayavishoni@gmail.com>
2026-06-26 19:30:27 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num,0)+1
+            if freq[num]  > n//2:
+                return num
+    
+
+
+    
